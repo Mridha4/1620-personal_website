c3c558b9585fbb8f207d233f541edcfd235b2ce1
Saad Al-Mridha <salmridha44@gmail.com>
2022-11-09 12:32:04 -0800

    Array called courseList and 2 items added

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+let courseList = {
+    code: "ACIT 1620",
+    name: "Web Technologies",
+};
+console.log(courseList);
+
+// function test() {
+//     prompt('What is you name?');
+//   }
+// test()
+  
